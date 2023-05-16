0c36e1d20643c03e1a1d88cb3128b1e470748898
Sandraaa <awuor.arunga@gmail.com>
2023-05-16 11:50:22 +0300

    using the sensor

--- senddatatoadafruit.py
+++ senddatatoadafruit.py
@@ -4,18 +4,24 @@ from time import sleep
 import machine
 from umqtt.simple import MQTTClient
 import random
+import ahtx0
+from machine import Pin, I2C
 
 import wificonnect
+#will only print after wificonnect works
 print("Connected to Wifi.")
-wlan = network.WLAN(network.STA_IF)
 
+wlan = network.WLAN(network.STA_IF)
 mqtt_server = 'io.adafruit.com'
 mqtt_port = 1883 # non-SSL port
 mqtt_user = 'arungasandie' #Adafruit ID
-mqtt_password = 'aio_RiLe85Zx3GYmXnV49Y8OL5BKvP7m' # Under Keys
+mqtt_password = 'aio_caWq92ETlBSemfFjQNBkQ2LrV4LI' # Under Keys
 mqtt_topic = 'arungasandie/feeds/temperature' # Under "Feed info"
+#adafruit needs different clients using their own unique names
 mqtt_client_id = str(random.randint(10000,999999)) #must have a unique ID - good enough for now
 
+i2c=I2C(1, scl=Pin(15), sda=Pin(14))
+sensor=ahtx0.AHT10(i2c)
 
 def mqtt_connect():
     client = MQTTClient(client_id=mqtt_client_id, server=mqtt_server, port=mqtt_port, user=mqtt_user, password=mqtt_password, keepalive=3600)
@@ -29,13 +35,15 @@ def reconnect():
     reset()
 
 try:
-    client = mqtt_connect()
+     client = mqtt_connect()
 except OSError as e:
     reconnect()
     
 while True:
     if wlan.isconnected():
-        client.publish(mqtt_topic, '10')
+        temp=str(round(sensor.temperature,1))
+        client.publish(mqtt_topic, temp)
+        print("Sent temp is "+ temp)
     else:
         reconnect()
-    sleep(20)
+    sleep(20)#send after 20 seconds to reduce data being collected
