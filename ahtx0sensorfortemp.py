bf9603bc828e9f28ccd454d9d9d4ca217fa7240e
Sandraaa <awuor.arunga@gmail.com>
2023-05-16 10:34:28 +0300

    temperature

--- ahtx0sensorfortemp.py
+++ ahtx0sensorfortemp.py
@@ -5,6 +5,6 @@ i2c=I2C(1, scl=Pin(15), sda=Pin(14))
 sensor=ahtx0.AHT10(i2c)
 
 while True:
-    #print(sensor.temperature)
-    print(sensor.relative_humidity)
+    print(sensor.temperature)
+    #print(sensor.relative_humidity)
     sleep(1)
